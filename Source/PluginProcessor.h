91cf0c90122333984cf62a0392e3720717d757f9
Orel Yefet <orelyefet@gmail.com>
2023-08-11 14:31:26 +0300

    Create new delayBuffer

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -57,6 +57,7 @@ public:
     void setStateInformation (const void* data, int sizeInBytes) override;
 
 private:
+    juce::AudioBuffer<float> delayBuffer;
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleDelayAudioProcessor)
 };
